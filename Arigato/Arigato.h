9d76e1ff6ad3587428663e0d5cfef7788f7af191
acb <acb@staticplain.local>
2020-04-20 16:33:27 +0200

    Initial Commit

---
+++ Arigato/Arigato.h
@@ -0,0 +1,19 @@
+//
+//  Arigato.h
+//  Arigato
+//
+//  Created by acb on 2020-04-20.
+//  Copyright © 2020 acb. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Arigato.
+FOUNDATION_EXPORT double ArigatoVersionNumber;
+
+//! Project version string for Arigato.
+FOUNDATION_EXPORT const unsigned char ArigatoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Arigato/PublicHeader.h>
+
+
